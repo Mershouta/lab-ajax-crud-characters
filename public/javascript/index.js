8ff7be9282abfa15ef6bc067e5bea44065fadb07
Mershouta <jchristkay@gmail.com>
2023-02-27 23:19:13 +0100

    Corrected

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,26 +1,160 @@
-  /**
-   * You might want to use this template to display each new characters
-   * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#examples
-   */
-  const characterTemplate = document.getElementById('template')
 
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+/**
+ * You might want to use this template to display each new characters
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template#examples
+ */
 
-  });
+const characterTemplate = document.querySelector('#template');
+const charactersContainer = document.querySelector(".characters-container");
+const fetchAllBtn = document.querySelector('#fetch-all');
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+const findByIdInput = document.querySelector("[name=character-id]");
+const fetchOneBtn = document.querySelector('#fetch-one');
 
-  });
+const deleteByIdInput = document.querySelector("[name=character-id-delete]");
+const deleteBtn = document.querySelector("#delete-one");
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+const addForm = document.querySelector("#new-character-form");
+const addNameInput = addForm.querySelector("[name=name]");
+const addOccupationInput = addForm.querySelector("[name=occupation]");
+const addWeaponInput = addForm.querySelector("[name=weapon]");
+const addCartoonInput = addForm.querySelector("[name=cartoon]");
+const addBtn = addForm.querySelector("#send-data");
 
-  });
+const updateForm = document.querySelector("#edit-character-form");
+const updateIdInput = updateForm.querySelector("[name=chr-id]");
+const updateNameInput = updateForm.querySelector("[name=name]");
+const updateOccupationInput = updateForm.querySelector("[name=occupation]");
+const updateWeaponInput = updateForm.querySelector("[name=weapon]");
+const updateCartoonInput = updateForm.querySelector("[name=cartoon]");
+const updateBtn = updateForm.querySelector("#send-data");
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+const characterApi = axios.create({
+  baseURL: 'http://localhost:5005/api/characters',
+});
 
-  });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+fetchAllBtn.addEventListener('click', fetchAll);
 
-  });
+fetchOneBtn.addEventListener('click', fetchOne);
 
+deleteBtn.addEventListener('click', deleteOne);
+
+updateForm.addEventListener('submit', editOne);
+
+addForm.addEventListener('submit', addOne);
+
+
+
+async function fetchAll(event) {
+  try {
+    const { data } = await characterApi.get();
+
+    changeBtnColor(fetchAllBtn);
+
+    charactersContainer.innerHTML = '';
+    for (const character of data) {
+      createCharacter(character);
+    }
+  } catch (error) {
+    console.error(error);
+
+    changeBtnColor(fetchAllBtn, false);
+  }
+};
+
+async function fetchOne(event) {
+  try {
+    if (!findByIdInput.value.trim()) { // for when the input is empty or has only spaces
+      return changeBtnColor(fetchOneBtn, false);
+    }
+
+    const { data: character } = await characterApi.get(`/${findByIdInput.value}`);
+
+    changeBtnColor(fetchOneBtn);
+
+    charactersContainer.innerHTML = '';
+    createCharacter(character);
+  } catch (error) {
+    console.error(error);
+
+    changeBtnColor(fetchOneBtn, false);
+  }
+}
+
+async function deleteOne(event) {
+  try {
+    event.preventDefault();
+    await characterApi.delete(`/${deleteByIdInput.value}`);
+
+    changeBtnColor(deleteBtn);
+    fetchAll();
+  } catch (error) {
+    console.error(error);
+
+    changeBtnColor(deleteBtn, false);
+  }
+}
+
+async function editOne(event) {
+  try {
+    event.preventDefault();
+    await characterApi.patch(
+      `/${updateIdInput.value}`,
+      {
+        name: updateNameInput.value,
+        occupation: updateOccupationInput.value,
+        weapon: updateWeaponInput.value,
+        cartoon: updateCartoonInput.checked
+      }
+    );
+
+    changeBtnColor(updateBtn);
+    fetchAll();
+  } catch (error) {
+    console.error(error);
+
+    changeBtnColor(updateBtn, false);
+  }
+}
+
+async function addOne(event) {
+  try {
+    event.preventDefault();
+    const { data: character } = await characterApi.post(
+      "/",
+      {
+        name: addNameInput.value,
+        occupation: addOccupationInput.value,
+        weapon: addWeaponInput.value,
+        cartoon: addCartoonInput.checked,
+      }
+    );
+
+    changeBtnColor(addBtn);
+    fetchAll();
+  } catch (error) {
+    console.log(error);
+
+    changeBtnColor(addBtn, false);
+  }
+}
+
+
+function createCharacter(character) {
+  const clone = characterTemplate.content.cloneNode(true);
+  clone.querySelector('.character-id span').textContent = character._id;
+  clone.querySelector('.name span').textContent = character.name;
+  clone.querySelector('.occupation span').textContent = character.occupation;
+  clone.querySelector('.cartoon span').textContent = character.cartoon;
+  clone.querySelector('.weapon span').textContent = character.weapon;
+
+  charactersContainer.append(clone);
+}
+
+function changeBtnColor(btnElement, success = true) {
+  btnElement.style.backgroundColor = success ? "green" : "red";
+  setTimeout(() => {
+    btnElement.style.backgroundColor = null;
+  }, 400);
+}
--- routes/characters.route.js
+++ routes/characters.route.js
@@ -1,45 +1,110 @@
-const router = require('express').Router()
-const Character = require('../models/Character.model')
-/**
- * !All the routes here are prefixed with /api/characters
- */
 
-/**
- * ? This route should respond with all the characters
- */
-router.get('/', (req, res, next) => {
-	/**Your code goes here */
-})
+const router = require('express').Router();
 
-/**
- * ? This route should create one character and respond with
- * ? the created character
- */
-router.post('/', (req, res, next) => {
-	/**Your code goes here */
-})
+const mongoose = require('mongoose');
+const Character = require('../models/Character.model');
 
-/**
- * ? This route should respond with one character
- */
-router.get('/:id', (req, res, next) => {
-	/**Your code goes here */
-})
+router.get('/', async (req, res, next) => {
+	try {
+		const allCharacters = await Character.find();
+		res.json(allCharacters);
+	} catch (error) {
+		next(error);
+	}
+});
 
-/**
- * ? This route should update a character and respond with
- * ? the updated character
- */
-router.patch('/:id', (req, res, next) => {
-	/**Your code goes here */
-})
+router.post('/', async (req, res, next) => {
+	try {
+		const { name, occupation, weapon, cartoon } = req.body;
 
-/**
- * ? Should delete a character and respond with a success or
- * ? error message
- */
-router.delete('/:id', (req, res, next) => {
-	/**Your code goes here */
-})
+		const wrongFields = {};
 
-module.exports = router
+		for ([key, value] of Object.entries({ name, occupation, weapon })) {
+			if (typeof value !== "string") wrongFields[key] = value;
+		}
+
+		if (Object.keys(wrongFields).length) {
+			return res.status(400).json({ wrongFields });
+		}
+
+		const createdCharacter = await Character.create({
+			name,
+			occupation,
+			weapon,
+			cartoon: Boolean(cartoon),
+		});
+		res.status(201).json(createdCharacter);
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.get('/:id', async (req, res, next) => {
+	try {
+		const { id } = req.params;
+
+		let oneCharacter = null;
+
+		if (mongoose.isValidObjectId(id)) {
+			oneCharacter = await Character.findById(id);
+		}
+
+		if (oneCharacter) {
+			res.json(oneCharacter);
+		} else {
+			res.status(404).json({ message: `No such character with id: ${id}` });
+		}
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.patch('/:id', async (req, res, next) => {
+	try {
+		const { id } = req.params;
+
+		let updatedCharacter = null;
+
+		if (mongoose.isValidObjectId(id)) {
+			const { name, occupation, cartoon, weapon } = req.body;
+
+			const newValues = {};
+
+			for ([key, value] of Object.entries({ name, occupation, weapon })) {
+				if (value && typeof value === "string") newValues[key] = value;
+			}
+
+			newValues.cartoon = Boolean(cartoon);
+
+			updatedCharacter = await Character.findByIdAndUpdate(
+				id,
+				newValues,
+				{ new: true }
+			);
+		}
+
+		if (updatedCharacter) {
+			res.json(updatedCharacter);
+		} else {
+			res.status(404).json({ message: `No such character with id: ${id}` });
+		}
+	} catch (error) {
+		next(error);
+	}
+});
+
+router.delete('/:id', async (req, res, next) => {
+	try {
+		const { id } = req.params;
+
+		if (mongoose.isValidObjectId(id)) {
+			await Character.findByIdAndDelete(id);
+		}
+
+		res.sendStatus(204);
+	} catch (error) {
+		next(error);
+	}
+});
+
+module.exports = router;
